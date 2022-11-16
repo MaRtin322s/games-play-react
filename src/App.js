df1c0343c8bbc6dd05c15243267b634846806041
martin13s18 <117728421+MaRtin322s@users.noreply.github.com>
2022-11-17 00:29:27 +0200

    paths navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
+import Catalog from './components/Catalog/Catalog';
 import Header from './components/common/Header';
+import Create from './components/Create/Create';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
 function App() {
     return (
-        <main id="main-content">
+        <div id="box">
             <Header />
-            <Home />
-        </main>
+            <main id="main-content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/logout" element={<Home />} />
+                    <Route path="/create" element={<Create />} />
+                    <Route path="/games" element={<Catalog />} />
+                </Routes>
+            </main>
+        </div>
     );
 }
 
