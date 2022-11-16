415a3fd07ca596b439307b3bc4b584e370795385
martin13s18 <117728421+MaRtin322s@users.noreply.github.com>
2022-11-16 23:58:02 +0200

    Header and Home components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Header from './components/common/Header';
+import Home from './components/Home/Home';
+
 function App() {
     return (
-        <div className="App">
-
-        </div>
+        <main id="main-content">
+            <Header />
+            <Home />
+        </main>
     );
 }
 
